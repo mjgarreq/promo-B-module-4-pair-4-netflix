8a3ceeab8eb094d75d3d3a6e4e368e5b8ea00fbe
AinaraHF <ainarahf@gmail.com>
2025-02-17 10:59:49 +0100

    endpoints class

--- src/index.js
+++ src/index.js
@@ -6,6 +6,35 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.get("/movies", (req, res)=>{
+  const fakeMovies = [
+    {
+      id: 1,
+      title: "Wonder Woman",
+      genre: "Action",
+      image:
+        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+      category: "Superhero",
+      year: 2017,
+      director: "Patty Jenkins",
+    },
+    {
+      id: 2,
+      title: "Inception",
+      genre: "Science Fiction",
+      image:
+        "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+      category: "Thriller",
+      year: 2010,
+      director: "Christopher Nolan",
+    },
+  ];
+  res.status(200).json({
+      success: true,
+      movies:  fakeMovies
+   })
+})
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
