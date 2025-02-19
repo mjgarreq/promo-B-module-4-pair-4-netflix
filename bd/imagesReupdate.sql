0430f8b53ba7c855045a391b302035fcc0a8e470
mjgarreq <majosegarcia96@gmail.com>
2025-02-19 11:35:33 +0100

    modified url images with error

---
+++ bd/imagesReupdate.sql
@@ -0,0 +1,4 @@
+SELECT * from movies;
+UPDATE movies SET image = "//localhost:4000/forrest_gump-212765827-mmed.jpg" WHERE idMovies=3;
+UPDATE movies SET image = "//localhost:4000/la_vita_e_bella-646167341-mmed.jpg" WHERE idMovies=2;
+UPDATE movies SET image = "//localhost:4000/pulp_fiction-210382116-large.jpg" WHERE idMovies=1;
